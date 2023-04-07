8996ca04ed2ca3c8bef45fdf2d6adc2792c81cba
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-07 13:16:43 +0300

    1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
 // import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
-import user from 'data/user.json';
+// import user from 'data/user.json';
 import data from 'data/data.json';
 
 const App = () => {
