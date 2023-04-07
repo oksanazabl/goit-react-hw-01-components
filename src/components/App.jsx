56fafee3e1fe61cc4db99f7b67211015e43709a5
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-07 11:12:31 +0300

    update APP

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
+import Profile from './Profile/Profile';
+import Statistics from './Statistics/Statistics';
+import user from 'data/user.json';
+import data from 'data/data.json';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        name={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <Statistics stats={data} /> */}
     </div>
   );
 };
+
+export default App;
