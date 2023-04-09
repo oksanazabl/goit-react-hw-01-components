1acada0fe873ccba60a7610ab762d9952012cbbc
OksanaZabl <73551722+oksanazabl@users.noreply.github.com>
2023-04-09 15:42:10 +0300

    end App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,22 +5,31 @@ import TransactionHistory from './Transaction/TransactionHistory';
 import user from 'data/user.json';
 import data from 'data/data.json';
 import friends from 'data/friends.json';
-import transactions from 'data/transactions.json'
+import transactions from 'data/transactions.json';
+import Container from './Container/Container';
 
 const App = () => {
   return (
-    <div>
-      <Profile
-        name={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={data} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />;
-    </div>
+    <>
+      <Container>
+        <Profile
+          name={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+      </Container>
+      <Container>
+        <Statistics title="Upload stats" stats={data} />
+      </Container>
+      <Container>
+        <FriendList friends={friends} />
+      </Container>
+      <Container>
+      <TransactionHistory items={transactions} />
+      </Container>
+    </>
   );
 };
 
